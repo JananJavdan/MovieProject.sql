a77524e8aa2c843619ee393d2f612b6b5307ad11
Janan Javdan <Fatemeh.Javdan@student.intecbrussel.be>
2024-03-14 15:56:32 +0100

    Add files via upload

--- MovieProject/src/main/java/moviedbmysqlproject/MessageTable.sql
+++ MovieProject/src/main/java/moviedbmysqlproject/MessageTable.sql
@@ -1,13 +1,21 @@
+use moviedb;
 CREATE TABLE message (
     msg_id INT AUTO_INCREMENT,
     message VARCHAR(255),
     user_id INT NOT NULL ,
-    receiver KEY INT NOT NULL,
+    receiver_id  INT NOT NULL,
     PRIMARY KEY (msg_id)
 
 );
 INSERT INTO message (message, user_id, receiver_id)
 VALUES ('Archchancelor: "Bursar?" Bursar: "Yes, Archchancellor?" Archchancelor:'
-    t a member of some secret society or somethin ', are you?" Bursar:' ||
+    " you aren't a member of some secret society or somethin ', are you?" 'Bursar:' ||
     ' "Me? No, Archchancellor." Archchancelor: "Then it\'d be a damn good
-    idea to take your underpants off your head;
+    idea to take your underpants off your head."', 3, 2); ||
+    ' "You aren 't a member of some secret society or somethin' ||
+    ''t a member of some secret society or somethin ', are you?" Bursar: ' ||
+    '"Me? No, Archchancellor." Archchancelor: "Then it 'd be a damn good
+    idea to take your underpants off your head."', 3, 2); ||
+    ' "You aren 't a member of some secret society or somethin ',
+    are you?" Bursar: "Me? No, Archchancellor." Archchancelor:
+    "Then it 'd be a damn good idea to take your underpants off your head."', 3, 2);
